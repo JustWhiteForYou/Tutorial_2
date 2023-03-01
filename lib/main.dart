a3f7972a5a9ec9b352e64ce9c5b20d0e31f04337
JustWhiteForYou <zijrfe2@gmail.com>
2023-03-01 16:30:15 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,81 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class Car {
   final String title;
   final String description;
   final int price;
 
-  Car(this.title,this.description,this.price);
+  Car(this.title, this.description, this.price);
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   final List<Car> _cars = [
     Car('BMW', 'Germany Car', 15000),
     Car("Toyota", 'Japan Car', 20000),
     Car('Renault', 'French Car', 10000),
   ];
 
-  void _addCar(){
-    setState((){
-      _cars.add(
-          Car('Unknown', 'model', 404));
-    });
+  late String _newTitle;
+  late String _newDescription;
+  late int _newPrice;
+
+  void _addCar() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add a Car'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  _newTitle = value;
+                },
+                decoration: InputDecoration(labelText: 'Title'),
+              ),
+              TextField(
+                onChanged: (value) {
+                  _newDescription = value;
+                },
+                decoration: InputDecoration(labelText: 'Description'),
+              ),
+              TextField(
+                onChanged: (value) {
+                  _newPrice = value.isEmpty ? 0 : int.parse(value);
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Price'),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  _cars.add(Car(
+                    _newTitle,
+                    _newDescription,
+                    _newPrice,
+                  ));
+                });
+              },
+              child: Text('Add'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('End'),
+            ),
+          ],
+        );
+      },
+    );
   }
+
+
 
   void _deleteCar(int index) {
     setState(() {
@@ -44,46 +98,47 @@ class _MyAppState extends State<MyApp>{
             actions: <Widget>[
               ElevatedButton(
                   onPressed: () => Navigator.of(context).pop(),
-                  child: Text('End this'))
+                  child: const Text('End this'))
             ],
           );
         });
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title:Text('Wlecome to the club'),
-          actions:[
+          title: Text('Welcome to the club'),
+          actions: [
             IconButton(
-                onPressed: _addCar,
-                icon: const Icon(
-                  Icons.add,
-                  size: 38,
-                  color: Colors.yellow,)
+              onPressed: _addCar,
+              icon: const Icon(
+                Icons.add,
+                size: 38,
+                color: Colors.yellow,
+              ),
             ),
           ],
         ),
         body: ListView.builder(
-            itemCount:_cars.length,
-            itemBuilder:(BuildContext context, int index) {
+            itemCount: _cars.length,
+            itemBuilder: (BuildContext context, int index) {
               return ListTile(
                 title: Text(_cars[index].title),
                 subtitle: Text('\₴${_cars[index].price}'),
                 onTap: () => _viewCar(context, _cars[index]),
-                trailing:IconButton(
-                    onPressed:() => _deleteCar(index),
-                    icon: const Icon(
-                      Icons.delete,
-                      size: 26,
-                      color: Colors.orange,
-                    ),
+                trailing: IconButton(
+                  onPressed: () => _deleteCar(index),
+                  icon: const Icon(
+                    Icons.delete,
+                    size: 26,
+                    color: Colors.orange,
+                  ),
                 ),
               );
-            }
-        ),
+            }),
       ),
     );
   }
